2d71db0d1412e69df8dc5ebda02c18ee15011a05
Johannes Goslar <jogo@kronberger-spiele.de>
2016-09-12 15:35:11 +0200

    t3kit content

---
+++ ext_emconf.php
@@ -0,0 +1,29 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "introduction".
+#
+# Auto generated 10-11-2015 15:19
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'The official Introduction Package',
+	'description' => 'This package delivers a new website (page tree) and shows all out-of-the-box features of TYPO3, and includes a theme based on Twitter Bootstrap 3, and a style editor to customize the design. Sample editors and backend groups are added',
+	'category' => 'distribution',
+    'version' => '1.0.0',
+	'state' => 'stable',
+	'clearcacheonload' => 1,
+    'author' => 'Johannes Goslar',
+    'author_email' => 'johannes.goslar@dkd.de',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '7.6.0-8.9.99'
+        ),
+        'conflicts' => array(),
+        'suggests' => array()
+    )
+);
